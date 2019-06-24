3bb48aa19dd44b4b3368a0d61d90612f780c3790
Arjun <arjunpatel555@gmail.com>
2019-06-24 21:25:35 +1000

    created location controller

---
+++ controllers/locationCtrl.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv')
+const axios = require('axios')
+dotenv.config()
+
+// API URL Builder
+function openCageUrl(placeName) {
+    let baseUrl = `https://api.opencagedata.com/geocode/v1/json?`
+    return `${baseUrl}q=${placeName}&key=${process.env.OPEN_CAGE_API_KEY}&language=en&pretty=1`
+}
+
+// Returns Location Data for city
+async function getLocationData(city){
+    console.log(`Searching for location: ${city}`)
+    var {data} = await axios.get(openCageUrl(city))
+    console.log(`Data is: ${data.results}`)
+    return data.results[0]
+}
+
+module.exports = {
+    "getLocationData": getLocationData
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080;
 const dotenv = require('dotenv')
-const axios = require('axios')
+const {getLocationData} = require('./controllers/locationCtrl')
 dotenv.config()
 
 app.listen(port, () =>{
@@ -14,32 +14,12 @@ app.use(express.static('client/build'));
 
 // An api endpoint that returns a short list of items
 app.get('/api/getLocation', (req,res) => {
-    console.log(`Retrieving Location lat/lon...`)
-    let location = {
-        city: "london",
-        coordinates: {lat: 0, lng: 0}
-    }
-
-    getLocation(location.city)
+    let city = "london"
+    getLocationData(city)
         .then(
-            (res1) => {
-                location.coordinates = res1.data.results[0].geometry
-                console.log(res1.data.results[0].geometry)
-                res.json(location)
+            (result) => {
+                console.log(result)
+                res.json(result)
             }
         )
-        .catch(
-            (err) => console.log(err)
-        )
 });
-
-
-// get the lat and long of the specified place name
-function openCageUrl(placeName) {
-    let baseUrl = `https://api.opencagedata.com/geocode/v1/json?`
-    return `${baseUrl}q=${placeName}&key=${process.env.OPEN_CAGE_API_KEY}&language=en&pretty=1`
-}
-
-function getLocation(city){
-    return axios.get(openCageUrl(city))
-}
